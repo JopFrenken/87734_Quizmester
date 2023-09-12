06923e5a4e325f0ea80a57afd7c846eab815b0ae
JopFrenken <87734@roc-teraa.nl>
2023-09-12 09:18:59 +0200

    project startup

--- 87734_Quizmester/Form1.Designer.cs
+++ 87734_Quizmester/Form1.Designer.cs
@@ -29,13 +29,120 @@ namespace _87734_Quizmester
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtRegUsername = new System.Windows.Forms.TextBox();
+            this.txtRegPassword = new System.Windows.Forms.TextBox();
+            this.lblRegUsername = new System.Windows.Forms.Label();
+            this.lblRegPassword = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLoginInstead = new System.Windows.Forms.Button();
+            this.lblEntryScreen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.Black;
+            this.lblWelcome.Location = new System.Drawing.Point(174, 30);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(452, 37);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome to the Nintendo Quiz";
+            // 
+            // txtRegUsername
+            // 
+            this.txtRegUsername.Location = new System.Drawing.Point(276, 194);
+            this.txtRegUsername.Name = "txtRegUsername";
+            this.txtRegUsername.Size = new System.Drawing.Size(188, 20);
+            this.txtRegUsername.TabIndex = 1;
+            // 
+            // txtRegPassword
+            // 
+            this.txtRegPassword.Location = new System.Drawing.Point(276, 267);
+            this.txtRegPassword.Name = "txtRegPassword";
+            this.txtRegPassword.Size = new System.Drawing.Size(188, 20);
+            this.txtRegPassword.TabIndex = 2;
+            // 
+            // lblRegUsername
+            // 
+            this.lblRegUsername.AutoSize = true;
+            this.lblRegUsername.Location = new System.Drawing.Point(273, 167);
+            this.lblRegUsername.Name = "lblRegUsername";
+            this.lblRegUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblRegUsername.TabIndex = 3;
+            this.lblRegUsername.Text = "Username";
+            // 
+            // lblRegPassword
+            // 
+            this.lblRegPassword.AutoSize = true;
+            this.lblRegPassword.Location = new System.Drawing.Point(273, 239);
+            this.lblRegPassword.Name = "lblRegPassword";
+            this.lblRegPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblRegPassword.TabIndex = 4;
+            this.lblRegPassword.Text = "Password";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(276, 325);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(188, 29);
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnLoginInstead
+            // 
+            this.btnLoginInstead.Location = new System.Drawing.Point(276, 399);
+            this.btnLoginInstead.Name = "btnLoginInstead";
+            this.btnLoginInstead.Size = new System.Drawing.Size(188, 29);
+            this.btnLoginInstead.TabIndex = 6;
+            this.btnLoginInstead.Text = "Login instead";
+            this.btnLoginInstead.UseVisualStyleBackColor = true;
+            this.btnLoginInstead.Click += new System.EventHandler(this.btnLoginInstead_Click);
+            // 
+            // lblEntryScreen
+            // 
+            this.lblEntryScreen.AutoSize = true;
+            this.lblEntryScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntryScreen.ForeColor = System.Drawing.Color.Black;
+            this.lblEntryScreen.Location = new System.Drawing.Point(294, 95);
+            this.lblEntryScreen.Name = "lblEntryScreen";
+            this.lblEntryScreen.Size = new System.Drawing.Size(157, 26);
+            this.lblEntryScreen.TabIndex = 7;
+            this.lblEntryScreen.Text = "Please register";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEntryScreen);
+            this.Controls.Add(this.btnLoginInstead);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblRegPassword);
+            this.Controls.Add(this.lblRegUsername);
+            this.Controls.Add(this.txtRegPassword);
+            this.Controls.Add(this.txtRegUsername);
+            this.Controls.Add(this.lblWelcome);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtRegUsername;
+        private System.Windows.Forms.TextBox txtRegPassword;
+        private System.Windows.Forms.Label lblRegUsername;
+        private System.Windows.Forms.Label lblRegPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLoginInstead;
+        private System.Windows.Forms.Label lblEntryScreen;
     }
 }
 
--- 87734_Quizmester/Form1.cs
+++ 87734_Quizmester/Form1.cs
@@ -12,9 +12,68 @@ namespace _87734_Quizmester
 {
     public partial class Form1 : Form
     {
+        // create new buttons dynamically
+        Button btnLogin = null;
+        Button btnRegInstead = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // creates the new items once the form loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnLogin = new Button();
+            btnRegInstead = new Button();
+            btnLogin.Text = "Login";
+            btnRegInstead.Text = "Register instead";
+
+            // Add the new buttons to the form
+            this.Controls.Add(btnLogin);
+            this.Controls.Add(btnRegInstead);
+
+            // hidden for now
+            btnLogin.Visible = false;
+            btnRegInstead.Visible = false;
+
+            // Attach the event handlers to the buttons
+            /*btnLogin.Click += new EventHandler(btnLoginInstead_Click);*/
+            btnRegInstead.Click += new EventHandler(btnRegInstead_Click);
+        }
+
+        private void btnLoginInstead_Click(object sender, EventArgs e)
+        {
+            // set the location and size of the new buttons to match the existing buttons
+            btnLogin.Location = btnRegister.Location;
+            btnRegInstead.Location = btnLoginInstead.Location;
+            btnLogin.Size = btnRegister.Size;
+            btnRegInstead.Size = btnLoginInstead.Size;
+
+            // Hide the existing buttons
+            btnRegister.Visible = false;
+            btnLoginInstead.Visible = false;
+
+            // show the other buttons
+            btnLogin.Visible = true;
+            btnRegInstead.Visible = true;
+
+            // Change label text
+            lblEntryScreen.Text = "Please login";
+        }
+
+        private void btnRegInstead_Click(object sender, EventArgs e)
+        {
+            // Hide the existing buttons
+            btnLogin.Visible = false;
+            btnRegInstead.Visible = false;
+
+            // Adds back the other buttons
+            btnRegister.Visible = true;
+            btnLoginInstead.Visible = true; 
+
+            // Change label text
+            lblEntryScreen.Text = "Please register";
         }
     }
 }
